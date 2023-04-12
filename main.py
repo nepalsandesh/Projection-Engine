d82d8f8111f1a66dff0f19a3cb1244894b898e77
nepalsandesh <sandeshnepal43@gmail.com>
2023-04-12 09:54:15 +0545

    all files modified

--- main.py
+++ main.py
@@ -1,53 +1,66 @@
-import pygame
+import pygame 
 from constants import *
+from ui import Button
+from screens.chaos import Chaos
 
 
-
-def main():
-    """Main function for running the System"""
-    # initialize pygame
-    pygame.init()
-    screen = pygame.display.set_mode(RESOLUTION)
-    pygame.display.set_caption('Projection Engine')
-    clock = pygame.time.Clock()
-    
-    engine = RenderEngine(screen, clock, 60)
-    engine.run()
-    
-    pygame.quit()    
-    
-
-class RenderEngine:
-    """Main Rendering Engine class"""
-    def  __init__(self, screen, clock, FPS=60):
-        self.screen = screen
-        self.clock = clock
-        self.FPS = FPS
-        n_height = HEIGHT/4
+class Render:
+    """Main Rendering class"""
+    def __init__(self):
+        pygame.init()
+        self.WIDTH = 1920
+        self.HEIGHT = 1080
+        self.RESOLUTION = (self.WIDTH, self.HEIGHT)
+        self.screen = pygame.display.set_mode(self.RESOLUTION)
+        self.clock = pygame.time.Clock()
+        self.FPS = 60
         self.running = True
         
-    def clearScreen(self):
-        self.screen.fill(BLACK)
+        # BUTTONS
+        self.ChaosEquationButton = Button(WIDTH//2 - 100, self.HEIGHT//3, 200, 60, "Chaos Equation")
+        self.SimulationButton = Button(WIDTH//2 - 100, self.HEIGHT//3 + 100, 200, 60, "Simulations")
+        self.MathEngineButton = Button(WIDTH//2 - 100, self.HEIGHT//3 + 200, 200, 60, "Math Engine")
+        self.ObjRenderButton = Button(WIDTH//2 - 100, self.HEIGHT//3 + 300 , 200, 60, "Object Render")
+        self.AlgVizButton = Button(WIDTH//2 - 100, self.HEIGHT//3 + 400 , 200, 60, "Algorithms Visualization")
         
-    def handleEvent(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
         
+        # SCREENS
+        self.chaosScreen = Chaos(self.screen, self.clock)
     
+    def clear_screen(self):
+        self.screen.fill((0,0,0))
+        
+    def displayMainMenu(self):
+        if self.ChaosEquationButton.Draw(self.screen):
+            self.chaosScreen.Run()
+        elif self.SimulationButton.Draw(self.screen):
+            pass
+        elif self.MathEngineButton.Draw(self.screen):
+            pass
+        elif self.ObjRenderButton.Draw(self.screen):
+            pass
+        elif self.AlgVizButton.Draw(self.screen):
+            pass
+    
+    def check_events(self):
+        [exit() for event in pygame.event.get() if event.type == pygame.QUIT]
+        
     def run(self):
         while self.running:
             self.clock.tick(self.FPS)
-            self.clearScreen()
-            self.handleEvent()
+            self.clear_screen()
+            self.check_events()
+            
+            self.displayMainMenu()
             
             pygame.display.update()
             
+
+
             
-if __name__ == '__main__':
-    main()
+            
             
         
+if __name__ == "__main__":
+    app = Render()
+    app.run()
--- ui.py
+++ ui.py
@@ -1,61 +1,57 @@
 import pygame
 from constants import *
 
+def RenderText(screen, message, font, text_color=WHITE, x=WIDTH//2, y=HEIGHT//2):
+    img = font.render(message, True, text_color)
+    rect = img.get_rect()
+    rect.center = (x, y)
+    screen.blit(img, rect)
 
 class Button:
-    """A Button UI class
-    parameters: x-pos, y-pos, width, height, text
-    """
+    """Button Class having multiple methods"""
     def __init__(self, x, y, w, h, text):
         self.rect = pygame.Rect(x, y, w, h)
         self.w = w
-        self.h = h 
+        self.h = h
         self.text = text
         self.font_size = 20
-        
-        self.is_clicked = False
+
+        self.isClicked = False
         self.action = False
         
         self.color = (200, 200, 200)
         self.hover_color = (150, 150, 150)
         self.initial_color = self.color
-        self.text_color = BLACK
+        self.text_color = (0,0,0)
+        self.InitializeFont()
         
-        self.initializeFont()
+    def InitializeFont(self):
+        self.font = pygame.font.SysFont("consolas", self.font_size)
         
-    def initializeFont(self):
-        self.font = pygame.font.SysFont('consolas', self.font_size)
-        
-    def setColor(self, new_color):
-        self.color = new_color
-        self.initial_color = new_color
-        
-    def update(self):
+    
+    def Update(self):
         action = False
         m_pos = pygame.mouse.get_pos()
+        # check mouseover and clicked conditions
         if self.rect.collidepoint(m_pos):
             self.color = self.hover_color
-            if pygame.mouse.get_pressed()[0] and self.is_clicked == False:
+            if pygame.mouse.get_pressed()[0] and self.clicked == False:
                 self.color = (255, 255, 0)
                 action = True
-                self.is_clicked = True
+                self.clicked = True
         else:
             self.color = self.initial_color
         if pygame.mouse.get_pressed()[0] == 0:
-            self.is_clicked = False
+            self.clicked = False
         
         return action
     
-    
-    def RenderText(screen, message, font, text_color=WHITE, x=WIDTH//2, y=HEIGHT//2):
-        img = font.render(message, True, text_color)
-        rect = img.get_rect()
-        rect.center = (x, y)
-        screen.blit(img, rect)
-    
-    def draw(self, screen):
-        action = self.update()
+    def Draw(self, screen):
+        action = self.Update()
         pygame.draw.rect(screen, self.color, self.rect)
-        
+        RenderText(screen, self.text, self.font, self.text_color, self.rect.x + self.w//2, self.rect.y + self.h//2)
+        return action
     
-    
+    def SetColor(self, new_color):
+        self.color = new_color
+        self.initial_color = new_color
